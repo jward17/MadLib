52a8a346d07efb231498c2b66d5fd1d365e94f45
jward17 <32141216+jward17@users.noreply.github.com>
2017-10-02 11:18:18 -0400

    Add files via upload

--- madlibs.js
+++ madlibs.js
@@ -1,19 +1,37 @@
+// Initialize variables
 var sentence=[];
 var replacements=[];
 var index=0;
+// initialize users
 alert ("Filler, Go Away.");
 alert ("End with shift+6(^)");
-//populate(index, sentence);
-alert (populate(index, sentence));
-
-
-function populate(sindex, sarray){
+// fill sentence
+populate(index, sentence, "word");
+alert (sentence);
+populate(index, replacements, "position");
+// fill or specify positions
+alert (replacements);
+replace(index, sentence, replacements, "part of speech");
+function populate(sindex, sarray, label){
 	do{
-		sarray[sindex]=prompt("Next word?");
+		sarray[sindex]=prompt("Next "+label+"?");
 		sindex = sindex+1;
 	}
 	while (sarray[sindex-1]!="^");
 	console.log(sarray);
 	sarray.splice(-1,1);
+	return sarray;
+}
+
+
+
+
+
+
+
+function replace(sindex, sarray, rarray, label) {
+	for sindex = 0; sindex < rarray.length; sindex++) {
+		sarray[rarray[sindex]] = prompt("next "+label+"?");
+	}
 	return sarray;
 }
